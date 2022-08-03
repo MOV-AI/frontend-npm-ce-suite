ef6052cf003d054d7be27f43a99c6dac16578cfd
Jose Sampaio <jandresampaio@gmail.com>
2022-08-03 11:34:42 +0000

    Add devcontainer and remove strict

--- apps/ide-ce/src/main.tsx
+++ apps/ide-ce/src/main.tsx
@@ -2,6 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App/App";
 import "./main.css";
+import { reportWebVitals } from "@mov-ai/ide-core";
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
 
 ReactDOM.render(
   <React.StrictMode>
--- libs/ide-core/src/index.ts
+++ libs/ide-core/src/index.ts
@@ -28,11 +28,6 @@ import ShortcutsPlugin, {
   getShortcutsTab,
 } from "./lib/tools/AppShortcuts/AppShortcuts";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
-
 // Export classes to build App
 export { BaseApp, installEditor, installTool };
 export { Store, DBSubscriber };
@@ -49,3 +44,4 @@ export { CONSTANTS };
 export { ApplicationTheme };
 export { HomeTabPlugin, getHomeTab };
 export { ShortcutsPlugin, getShortcutsTab };
+export { reportWebVitals };
